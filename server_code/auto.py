90a589de0d9f0ad65e54487ab72ef5e9ff806bf0
racersmith@gmail.com <racersmith@gmail.com>
2025-01-16 21:28:38 +0000

    Edited auto

--- server_code/auto.py
+++ server_code/auto.py
@@ -47,7 +47,7 @@ def _format_test_name(fn, test_module_name="tests"):
     return f"{module}.{fn.__qualname__}"
 
 
-def run(test_package):
+def run(test_package, quiet=True):
     log = list()
     log.append("== Starting Test ==")
     found_tests = _find_tests(test_package)
@@ -57,18 +57,24 @@ def run(test_package):
     passed = 0
     failed = 0
 
-    for fn in found_tests:
-        test_name = _format_test_name(fn, "tests")
+    for test in found_tests:
+        test_name = _format_test_name(test, "tests")
         try:
-            fn()
+            # Run the test
+            test()
             passed += 1
-            log.append(f"  Pass: {test_name}")
+            if not quiet:
+                log.append(f"  Pass: {test_name}")
 
+        # capture the assertion error from our test
         except AssertionError as e:
             log.append(f"> Fail: {test_name} - {e}")
             failed += 1
 
+    # Summary info
     log.append(f"\n{passed}/{n_tests} passed")
     log.append(f"{failed} failed tests")
 
+    # I found this more reliable for printing to the console.
+    # Otherwise, printing as I went the lines would stack, get out of order, etc.
     print("\n".join(log))
