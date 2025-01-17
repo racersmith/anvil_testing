feb5011de8d5e46dc598dfe4b4366ab2c45b0cf5
racersmith@gmail.com <racersmith@gmail.com>
2025-01-17 21:01:58 +0000

    Simplifying Testing

--- server_code/auto.py
+++ server_code/auto.py
@@ -2,6 +2,7 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 import inspect as _inspect
+from dataclasses import dataclass
 
 FN_PREFIX = "test_"
 CLS_PREFIX = "Test"
@@ -47,39 +48,75 @@ def _format_test_name(fn, test_module_name="tests"):
     return f"{module.replace('.', '/')}::{fn.__qualname__.replace('.', '::')}"
 
 
+@dataclass
+class TestResult:
+    success: bool
+    test_name: str
+    error: str = ""
+
+    def __str__(self):
+        if self.success:
+            return f"  Pass: {self.test_name}"
+        else:
+            return f"> Fail: {self.test_name} - {self.error}"
+
+    def __add__(self, other) -> int:
+        return other + int(self.success)
+
+    def __radd__(self, other) -> int:
+        if not other:
+            return int(self.success)
+        else:
+            return self.__add__(other)
+
+
+def _run_test(test) -> TestResult:
+    """Run a single test"""
+    test_name = _format_test_name(test, "tests")
+
+    try:
+        # Run the test
+        test()
+        return TestResult(True, test_name)
+
+    # capture the assertion error from our test
+    except AssertionError as e:
+        return TestResult(False, test_name, e)
+
+    # capture a run error to aid in debugging
+    # otherwise we just get the standard anvil runtime exception error.
+    except Exception as e:
+        return TestResult(False, test_name, f"{type(e).__name__}: {e}")
+
+
 def run(test_package, quiet=True):
+    """Run the test suite
+    Args:
+        test_package: module where the tests reside
+        quiet: True will only display failed tests, False will include passing tests in results
+    """
     log = list()
     log.append(f"{' Anvil Testing ':=^50s}")
     found_tests = _find_tests(test_package)
     n_tests = len(found_tests)
-    log.append(f"Found {n_tests} tests\n")
+    log.append(f"Collected {n_tests} tests\n")
 
-    passed = 0
-    failed = 0
+    # Run the collected tests
+    test_results = [_run_test(test) for test in found_tests]
 
-    for test in found_tests:
-        test_name = _format_test_name(test, "tests")
-        try:
-            # Run the test
-            test()
-            passed += 1
-            if not quiet:
-                log.append(f"  Pass: {test_name}")
-
-        # capture the assertion error from our test
-        except AssertionError as e:
-            log.append(f"> Fail: {test_name} - {e}")
-            failed += 1
+    # add results to output log
+    log.extend(str(result) for result in test_results)
 
     # Summary info
+    passed = sum(test_results)
+    failed = n_tests - passed
     log.append(f"\n{passed}/{n_tests} passed")
     log.append(f"{failed} failed tests")
-    result = ' PASS ' if not failed else ' FAIL '
+    result = " PASS " if not failed else " FAIL "
     log.append(f"{result:=^50s}")
 
     # I found this more reliable for printing to the console.
     # Otherwise, printing as I went the lines would stack, get out of order, etc.
-
     result = "\n".join(log)
     print(result)
     return result
