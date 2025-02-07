a5e4812ef1d0ed3da37675895dc9ad2e42bf7421
racersmith@gmail.com <racersmith@gmail.com>
2025-02-07 18:27:31 +0000

    Edited _testing._test

--- server_code/_testing/_test.py
+++ server_code/_testing/_test.py
@@ -4,8 +4,8 @@ import anvil.server
 Expose an endpoint to run test at when we are in a debug environment.
 """
 
+# When writing dependencies, add a check fo the app.id to avoid starting multiple endpoints
 if 'debug' in app.environment.tags and app.id == 'CCW3SYLSAQHLCF2A':
-    
     test_endpoint = '/test'
     print('Tests can be run here:')
     print(f"{anvil.server.get_app_origin('debug')}{test_endpoint}")
